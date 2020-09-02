ccc88a605cbf1ee8856ad7a6c05e75f1d05f1b2f
Tom Overland <tomoverland03@gmail.com>
2020-09-01 20:24:07 -0500

    added description, installation, and usage objects

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ const writeFile = util.promisify(fs.writeFile);
 
 //function that creates the array of questions for the user
 function promptUser() {
-  return inquirer.prompt({
-    type: "input",
-    message: "What is the name of your project?",
-    name: "title",
-  });
+  return inquirer.prompt(
+    {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Please enter a description for your project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message:
+        "What are the installation instructions for this project?  Write NONE if no instructions.",
+      name: "installation",
+    },
+    {
+        type: "input",
+        message: "How would you like your application to be used?";
+        name: "usage"
+    },
+    
+  );
 }
+
+promptUser();
