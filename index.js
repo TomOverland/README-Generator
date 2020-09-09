a621f80654edd40416552c3a45de0c2f894fa0d5
Tom Overland <tomoverland03@gmail.com>
2020-09-09 17:56:24 -0500

    removed function that calls questions, structured questions in an object instead

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import generateMarkdown from "./utils/generateMarkdown.js";
+import { prompt } from "inquirer";
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
@@ -5,70 +8,73 @@ const util = require("util");
 const writeFile = util.promisify(fs.writeFile);
 
 // This is a function that creates the array of questions for the user
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the name of your project?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Please enter a description for your project.",
-      name: "description",
-    },
-    {
-      type: "input",
-      message:
-        "What are the installation instructions for this project?  Write NONE if no instructions.",
-      name: "installation",
-    },
-    {
-      type: "input",
-      message: "How would you like your application to be used?",
-      name: "usage",
-    },
-    {
-      type: "input",
-      message: "How can people contribute to this project?",
-      name: "contributing",
-    },
-    {
-      type: "input",
-      message:
-        "What are the test instructions?  Write NONE if no test instructions.",
-      name: "tests",
-    },
-    {
-      type: "checkbox",
-      message: "Please select a license",
-      choices: ["Apache", "MIT", "ISC", "GNU GPLv3"],
-      name: "license",
-    },
-    {
-      type: "input",
-      message: "What is your email address?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What is your GitHub username?",
-      name: "username",
-    },
-  ]);
+const questions = [
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please enter a description for your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message:
+      "What are the installation instructions for this project?  Write NONE if no instructions.",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "How would you like your application to be used?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "How can people contribute to this project?",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message:
+      "What are the test instructions?  Write NONE if no test instructions.",
+    name: "tests",
+  },
+  {
+    type: "checkbox",
+    message: "Please select a license",
+    choices: ["Apache", "MIT", "ISC", "GNU GPLv3"],
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+];
+
+function writeToFile(fileName, data) {
+  writeFile(fileName, data, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
 }
 
 // Creating a function to initialize program
-async function init() {
-  try {
-    const response = await promptUser();
-    const readMe = generateMarkdown(response);
+function init() {
+  prompt(questions).then((answers) => {
+    const response = generateMarkdown(answers);
+    console.log(answers);
 
-    await writeFile("README.md", readMe);
-    console.log("File created successfully!");
-  } catch (err) {
-    console.log(err);
-  }
+    writeToFile("README.md", response);
+  });
 }
 
 // Calling init function to run the program
