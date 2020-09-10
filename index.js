44b87d440a3b957d48a674008a8b65081c6c8d66
Tom Overland <tomoverland03@gmail.com>
2020-09-10 10:05:42 -0500

    seperated generateMarkdown into a seperate js file

--- index.js
+++ index.js
@@ -1,120 +1,79 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
-
-const writeFile = util.promisify(fs.writeFile);
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // This is a function that creates the array of questions for the user
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the name of your project?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Please enter a description for your project.",
-      name: "description",
-    },
-    {
-      type: "input",
-      message:
-        "What are the installation instructions for this project?  Write NONE if no instructions.",
-      name: "installation",
-    },
-    {
-      type: "input",
-      message: "How would you like your application to be used?",
-      name: "usage",
-    },
-    {
-      type: "input",
-      message: "How can people contribute to this project?",
-      name: "contributing",
-    },
-    {
-      type: "input",
-      message:
-        "What are the test instructions?  Write NONE if no test instructions.",
-      name: "tests",
-    },
-    {
-      type: "checkbox",
-      message: "Please select a license",
-      choices: ["Apache", "MIT", "ISC", "GNU GPLv3"],
-      name: "license",
-    },
-    {
-      type: "input",
-      message: "What is your email address?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What is your GitHub username?",
-      name: "username",
-    },
-  ])
+const questions = [
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please enter a description for your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message:
+      "What are the installation instructions for this project?  Write NONE if no instructions.",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "How would you like your application to be used?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "How can people contribute to this project?",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message:
+      "What are the test instructions?  Write NONE if no test instructions.",
+    name: "tests",
+  },
+  {
+    type: "checkbox",
+    message: "Please select a license",
+    choices: ["Apache", "MIT", "ISC", "GNU GPLv3"],
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+];
+
+// function to write file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("README.md was created");
+  });
 }
 
-function generateMarkdown(response) {
-  // The user will need to delete the spacing on the markdown document, as my formatter puts four spaces before each line starting on line 64
-  return `
-    # ${response.title}
-    
-    ## Description:
-    ![License](https://img.shields.io/badge/License-${response.license}-blue.svg "License Badge")
-    ${response.description}
+// function to initialize program
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const response = generateMarkdown(answers);
+    console.log(answers);
 
-    # Table of Contents
-    
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Contributing](#contributing)
-    - [Test](#tests)
-    - [License](#license)
-    - [Questions](#questions)
-    
-    ## Installation:
-    ${response.installation}
-
-    ## Usage:
-    ${response.usage}
-
-    ## License:
-    For more information about the license, click the link below:
-
-    - [License](https://opensource.org/licenses/${response.license})
-
-    ## Contributing:
-    ${response.contributing}
-
-    ## Tests:
-    ${response.tests}
-
-    ## Questions:
-    For more information about the Generator, please reach out to me via my GitHub profile.
-
-    -[GitHub Profile](https://github.com/${response.username})
-
-    For any other questions, please send me an email at: ${response.email}.
-    `;
+    writeToFile("README.md", response);
+  })
 }
 
-// Creating a function to initialize program
-async function init() {
-  try {
-    const response = await promptUser();
-    const readMe = generateMarkdown(response);
 
-    await writeFile("README.md", readMe);
-    console.log("File created successfully!");
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-// Calling init function to run the program
+// function call to initialize program
 init();
